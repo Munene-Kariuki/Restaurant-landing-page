854d479150eaab2d37f2ca6efca1b822da0a0821
Denis <denismunene999@gmail.com>
2022-06-23 12:16:10 +0300

    add function to fetch data from server

--- src/script.js
+++ src/script.js
@@ -3,10 +3,21 @@
 
 
 
-
+//Event listeners
+document.addEventListener('DOMContentLoaded', initializer)
 
 
 
 
 
 //fetch data from server
+function getData() {
+    fetch('http://localhost:3000/menuItems')
+    .then((response) => response.json())
+    .then((data) => populateMenu(data))
+}
+
+function initializer() {
+    getData()
+}
+
